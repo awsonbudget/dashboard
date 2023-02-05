de60964309b0777a8110ac295b5d901db85898ec
Mack Song <geliuchang.song@mail.mcgill.ca>
2023-02-05 18:20:19 -0500

    add pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
