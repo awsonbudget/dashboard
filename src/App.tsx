39889a6e40df505806707f288164554393a9a226
Joey Yu <joey@itsjoeoui.com>
2023-02-15 20:35:14 -0500

    Need to find a better way to display this

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const App = () => {
         All Nodes
       </div>
 
-      <div className="pt-4 px-10 grid grid-cols-2 gap-8">
+      <div className="pt-4 px-10 grid grid-cols-1 gap-8">
         {nodes.map((node: NodeProps, i: number) => (
           <Node
             name={node.name}
